b353d97202a93f34bb75e1081da725c286af002e
Derrick <mereo83@gmail.com>
2023-09-22 00:17:08 +0400

    added try and catch in all codes

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const port = process.env.PORT || 7000; // You can specify any port you prefer
 
 const app = express();
 
-// Configure middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+try {
+  // Configure middleware
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
 
-app.use(session({
-  secret: 'your-secret-key',
-  resave: true,
-  saveUninitialized: true,
-}));
+  app.use(session({
+    secret: 'your-secret-key',
+    resave: true,
+    saveUninitialized: true,
+  }));
 
-// Import the studentArray from studentRoutes.js
-const { studentArray } = require('./routes/studentRoutes');
+  // Import the studentArray from studentRoutes.js
+  const { studentArray } = require('./routes/studentRoutes');
 
-// Routes
-const receptionistRoutes = require('./routes/receptionistRoutes');
-const studentRoutes = require('./routes/studentRoutes');
-const teacherRoutes = require('./routes/teacherRoutes');
-const principalRoutes = require('./routes/principalRoutes');
+  // Routes
+  const receptionistRoutes = require('./routes/receptionistRoutes');
+  const studentRoutes = require('./routes/studentRoutes');
+  const teacherRoutes = require('./routes/teacherRoutes');
+  const principalRoutes = require('./routes/principalRoutes');
 
-app.use('/receptionist', receptionistRoutes);
-app.use('/student', studentRoutes);
-app.use('/teacher', teacherRoutes);
-app.use('/principal', principalRoutes);
+  app.use('/receptionist', receptionistRoutes);
+  app.use('/student', studentRoutes);
+  app.use('/teacher', teacherRoutes);
+  app.use('/principal', principalRoutes);
 
-const port = process.env.PORT || 3000; // You can specify any port you prefer
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 
-// Export the app object
-module.exports = app;
+  // Export the app object
+  module.exports = app;
+} catch (error) {
+  console.error('An error occurred:', error);
+}
--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-// Teacher can update student detail by ID
-router.put('/update/:id', (req, res) => {
-  const studentId = req.params.id;
-  const updatedData = req.body; // Assuming you send updated data in the request body
-  // Implement code to update the student's details by ID
-  // Update student details in the database
-  // Send a response indicating success or failure
+try {
+  // Teacher can update student detail by ID
+  router.put('/update/:id', (req, res) => {
+    const studentId = req.params.id;
+    const updatedData = req.body; // Assuming you send updated data in the request body
+    // Implement code to update the student's details by ID
+    // Update student details in the database
+    // Send a response indicating success or failure
 
-  if (username === 'teacher') {
-    // User is authenticated
-    res.status(200).json({ message: `Student details updated for ID ${studentId}` });
-  } else {
-    // User is not authenticated
-    res.status(401).json({ message: 'Unauthorized' });
-  }
-  
+    if (username === 'teacher') {
+      // User is authenticated
+      res.status(200).json({ message: `Student details updated for ID ${studentId}` });
+    } else {
+      // User is not authenticated
+      res.status(401).json({ message: 'Unauthorized' });
+    }
+  });
 
-});
+  // Other CRUD routes for teachers
 
-// Other CRUD routes for teachers
-
-module.exports = router;
+  module.exports = router;
+} catch (error) {
+  console.error('An error occurred in the route:', error);
+}
