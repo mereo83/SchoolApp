4c6a58db6ebe9cc9df16d5d6cb3cdf393597c172
Derrick <mereo83@gmail.com>
2023-09-26 23:26:49 +0400

    added await,try,catch and promise

--- routes/principalRoutes.js
+++ routes/principalRoutes.js
@@ -1,76 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql2'); // Import the mysql2 library
-
-// Create a MySQL connection
-const connection = mysql.createConnection({
-  host: 'localhost', // Change to your MySQL server host
-  user: 'root',      // Your MySQL username
-  password: 'Blessme@12', // Your MySQL password
-  database: 'school_management', // Replace with your database name
-});
+const pool = require('../config/db'); // Import the connection pool from db.js
 
 // Principal can view student detail by ID
-router.get('/view/:id', (req, res) => {
-  const studentId = req.params.id;
-  
-  // Implement code to fetch and return the student's details by ID for the principal
-  // Fetch student details from the database
-  const sql = 'SELECT * FROM Students WHERE id = ?';
-  
-  connection.query(sql, [studentId], (err, results) => {
-    if (err) {
-      console.error('Error fetching student details:', err);
-      res.status(500).json({ message: 'Internal Server Error' });
-    } else {
-      if (results.length > 0) {
-        // Student details found
-        const student = results[0]; // Assuming the query returns a single student
-        if (req.session.username === 'principal') {
-          // User is authenticated
-          res.status(200).json({ message: `Student details for ID ${studentId} (Principal view)`, student });
-        } else {
-          // User is not authenticated
-          res.status(401).json({ message: 'Unauthorized' });
-        }
+router.get('/view/:id', async (req, res) => {
+  try {
+    const studentId = req.params.id;
+
+    // Implement code to fetch and return the student's details by ID for the principal
+    // Fetch student details from the database
+    const sql = 'SELECT * FROM Students WHERE id = ?';
+
+    const [results] = await pool.query(sql, [studentId]);
+
+    if (results.length > 0) {
+      // Student details found
+      const student = results[0]; // Assuming the query returns a single student
+      if (req.session.username === 'principal') {
+        // User is authenticated
+        res.status(200).json({ message: `Student details for ID ${studentId} (Principal view)`, student });
       } else {
-        // Student with the given ID was not found
-        res.status(404).json({ message: `Student with ID ${studentId} not found` });
+        // User is not authenticated
+        res.status(401).json({ message: 'Unauthorized' });
       }
+    } else {
+      // Student with the given ID was not found
+      res.status(404).json({ message: `Student with ID ${studentId} not found` });
     }
-  });
-});
-
-// Principal can update student detail by ID
-router.put('/update/:id', (req, res) => {
-  const studentId = req.params.id;
-  const updatedData = req.body; // Assuming you send updated data in the request body
-  // Implement code to update the student's details by ID for the principal
-  // Update student details in the database
-  // Send a response indicating success or failure
-  
-  if (req.session.username === 'principal') {
-    // User is authenticated
-    res.status(200).json({ message: `Student details updated for ID ${studentId} (Principal update)` });
-  } else {
-    // User is not authenticated
-    res.status(401).json({ message: 'Unauthorized' });
+  } catch (error) {
+    console.error('Error fetching student details:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
-// Principal can delete student by ID
-router.delete('/delete/:id', (req, res) => {
-  const studentId = req.params.id;
-  // Implement code to delete a student by ID for the principal
-  // Delete student from the database
-  // Send a response indicating success or failure
-  if (req.session.username === 'principal') {
-    // User is authenticated
-    res.status(200).json({ message: `Student deleted for ID ${studentId} (Principal delete)` });
-  } else {
-    // User is not authenticated
-    res.status(401).json({ message: 'Unauthorized' });
-  }
-});
+// Other CRUD routes for principals
 
 module.exports = router;
--- routes/receptionistRoutes.js
+++ routes/receptionistRoutes.js
@@ -1,61 +1,53 @@
 const express = require('express');
 const router = express.Router();
-const db =require('../config/db.js')
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-
-
-
-// Use session and cookie middleware
-router.use(
-  session({
-    secret: 'your-secret-key',
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-router.use(cookieParser());
+const pool = require('../config/db'); // Import the connection pool from db.js
 
 // Receptionist can register a student after 2 minutes
-router.post('/register_student', (req, res) => {
-  // Set the user role in the session directly (since there's no login mechanism)
-  req.session.user = 'receptionist';
+router.post('/register_student', async (req, res) => {
+  try {
+    // Set the user role in the session directly (since there's no login mechanism)
+    req.session.user = 'receptionist';
 
-  // Check if the "registrationAllowed" cookie exists and is set to 'true'
-  if (req.cookies.registrationAllowed === 'true' || !req.cookies.registrationAllowed) {
-    // Check if the user is authenticated as a receptionist in the session
-    if (req.session.user === 'receptionist') {
-      // Receptionist can register a student
-      const newStudent = {
-        studid: req.body.studid,
-        name: req.body.name,
-        age: req.body.age,
-        gender: req.body.gender,
-        email: req.body.email,
-      };
+    // Check if the "registrationAllowed" cookie exists and is set to 'true'
+    if (req.cookies.registrationAllowed === 'true' || !req.cookies.registrationAllowed) {
+      // Check if the user is authenticated as a receptionist in the session
+      if (req.session.user === 'receptionist') {
+        // Receptionist can register a student
+        const newStudent = {
+          studid: req.body.studid,
+          name: req.body.name,
+          age: req.body.age,
+          gender: req.body.gender,
+          email: req.body.email,
+        };
 
-      // Insert the new student into the Students table
-      const sql = 'INSERT INTO Students (studid, name, age, gender, email) VALUES (?, ?, ?, ?, ?)';
-      const values = [newStudent.studid, newStudent.name, newStudent.age, newStudent.gender, newStudent.email];
+        // Insert the new student into the Students table
+        const sql = 'INSERT INTO Students (studid, name, age, gender, email) VALUES (?, ?, ?, ?, ?)';
+        const values = [
+          newStudent.studid,
+          newStudent.name,
+          newStudent.age,
+          newStudent.gender,
+          newStudent.email,
+        ];
 
-      connection.query(sql, values, (err, results) => {
-        if (err) {
-          console.error('Error registering student:', err);
-          res.status(500).json({ message: 'Internal Server Error' });
-        } else {
-          // Registration was successful
-          // Set the "registrationAllowed" cookie to false for the next 2 minutes
-          res.cookie('registrationAllowed', 'false', { maxAge: 120000 });
-          res.status(200).json({ message: 'Student registered successfully.' });
-        }
-      });
+        await pool.query(sql, values); // Use await here to wait for the query to complete
+
+        // Registration was successful
+        // Set the "registrationAllowed" cookie to false for the next 2 minutes
+        res.cookie('registrationAllowed', 'false', { maxAge: 120000 });
+        res.status(200).json({ message: 'Student registered successfully.' });
+      } else {
+        // User is not authenticated as a receptionist
+        res.status(401).json({ message: 'Unauthorized' });
+      }
     } else {
-      // User is not authenticated as a receptionist
-      res.status(401).json({ message: 'Unauthorized' });
+      // Registration is not allowed yet
+      res.status(403).json({ message: 'Registration not allowed at the moment. Please try again later.' });
     }
-  } else {
-    // Registration is not allowed yet
-    res.status(403).json({ message: 'Registration not allowed at the moment. Please try again later.' });
+  } catch (error) {
+    console.error('Error registering student:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
